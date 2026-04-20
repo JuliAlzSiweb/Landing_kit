32e04d153ca93ff0f909043d94d5357565bf411f
julian <julian.alzate@siweb.es>
2026-04-20 12:21:48 +0200

    [FIX] telenofono de llamanos ponemos correcto

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,7 +9,7 @@ export function Header() {
           Siweb
         </a>
         <div className="site-header__actions">
-          <a className="site-header__btn site-header__btn--outline" href="tel:+34900000000">
+          <a className="site-header__btn site-header__btn--outline" href="tel:+34886020705">
             Llámanos
           </a>
           <CtaOpenModalLink className="site-header__btn site-header__btn--primary">Quiero mi bono</CtaOpenModalLink>
